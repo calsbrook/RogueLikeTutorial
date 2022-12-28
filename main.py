540493047a64b5113971020425c1a26e07e50837
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2022-12-27 19:08:55 -0800

    part 2 complete

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -19,6 +23,9 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
+    game_map = GameMap(map_width, map_height)
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -28,23 +35,11 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y, string="@")
-
-            context.present(root_console)
-
-            root_console.clear()
+            engine.render(console=root_console, context=context)
             
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
+            events = tcod.event.wait()
 
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player.move(dx=action.dx, dy=action.dy)
-                
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
